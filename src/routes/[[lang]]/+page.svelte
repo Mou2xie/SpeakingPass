5d1b5f7b5693132d70d5a472a5763c5dabd03661
taskforce404 <xieyongjie2012@163.com>
2024-06-20 16:04:56 +0800

    font adjustment

--- src/routes/[[lang]]/+page.svelte
+++ src/routes/[[lang]]/+page.svelte
@@ -15,7 +15,7 @@
             4: "Part2&3 topics",
             5: "Check",
             6: "If you have taken the IELTS speaking test, I believe you will agree that the most difficult part of the IELTS speaking test is not how to speak, but what to say. In the examination room, you need to fight your nervousness while answering questions you have never thought about in a limited time. It can be said that not knowing what to say is the biggest opponent of every IELTS speaking candidate.",
-            7: "Fortunately, the topics of IELTS speaking exams are recycled, which gives us the opportunity to understand the questions and think about the answers in advance. At speakingpass, we continue to collect and publish all the real IELTS speaking exam questions. Every topic may appear again in the next exam. I hope that by then, you can confidently and calmly give your own perfect answer.",
+            7: "Fortunately, the topics of IELTS speaking exams are recycled, which gives us the opportunity to understand the questions and think about the answers in advance. At SpeakingPass, we continue to collect and publish all the real IELTS speaking exam questions. Every topic may appear again in the next exam. I hope that by then, you can confidently and calmly give your own perfect answer.",
             8: "Last update: ",
         },
         {
@@ -25,7 +25,7 @@
             4: "Part 2&3 话题",
             5: "查看",
             6: "如果你参加过雅思口语考试，相信你会认同，雅思口语考试最难的不是怎么说，而是说什么。在考场上，你需要一边对抗自己的紧张情绪，一边在有限的时间里回答自己从来没有思考过的问题，可以说，不知道说什么是每个雅思口语考生最大的对手。",
-            7: "好在雅思口语考试的话题是会循环使用的，这给了我们提前了解问题，思考答案的机会。在speakingpass，我们持续收集并发布所有的雅思口语考试真题，每一个话题都有可能在下次考试中再次出现，希望到那时，你能够自信而从容地给出你自己的完美答案。",
+            7: "好在雅思口语考试的话题是会循环使用的，这给了我们提前了解问题，思考答案的机会。在SpeakingPass，我们持续收集并发布所有的雅思口语考试真题，每一个话题都有可能在下次考试中再次出现，希望到那时，你能够自信而从容地给出你自己的完美答案。",
             8: "最后更新: ",
         },
         {
@@ -35,7 +35,7 @@
             4: "Chủ đề phần 2&3",
             5: "Kiểm tra",
             6: "Nếu bạn đã tham gia kỳ thi nói IELTS, tôi tin bạn sẽ đồng ý rằng phần khó nhất của kỳ thi nói IELTS không phải là nói như thế nào mà là nói gì. Trong phòng thi, bạn cần phải chiến đấu với sự lo lắng của mình khi trả lời những câu hỏi mà bạn chưa từng nghĩ tới trong thời gian có hạn. Có thể nói, việc không biết phải nói gì chính là đối thủ lớn nhất của mọi thí sinh thi IELTS Nói.",
-            7: "May mắn thay, các chủ đề của bài thi Nói IELTS được tái sử dụng, giúp chúng ta có cơ hội hiểu câu hỏi và suy nghĩ trước về câu trả lời. Tại speakpass, chúng tôi tiếp tục sưu tầm và đăng tải tất cả các đề thi IELTS thực tế. Mỗi chủ đề có thể xuất hiện lại trong kỳ thi tiếp theo. Tôi mong rằng đến lúc đó, bạn có thể tự tin và bình tĩnh đưa ra câu trả lời hoàn hảo cho riêng mình.",
+            7: "May mắn thay, các chủ đề của bài thi Nói IELTS được tái sử dụng, giúp chúng ta có cơ hội hiểu câu hỏi và suy nghĩ trước về câu trả lời. Tại SpeakPass, chúng tôi tiếp tục sưu tầm và đăng tải tất cả các đề thi IELTS thực tế. Mỗi chủ đề có thể xuất hiện lại trong kỳ thi tiếp theo. Tôi mong rằng đến lúc đó, bạn có thể tự tin và bình tĩnh đưa ra câu trả lời hoàn hảo cho riêng mình.",
             8: "Cập nhật cuối cùng: ",
         },
     ];
@@ -79,13 +79,11 @@
             alt="icon"
         />
         <h1
-            class=" text-pramary text-center mt-2 px-4 text-[14px] md:text-[20px]"
+            class=" text-pramary text-center mt-2 px-4 text-[16px] md:text-[20px]"
         >
             {text[textIndex][1]}
         </h1>
-        <div
-            class=" text-center mt-5 mb-5 md:text-base text-pramary font-light"
-        >
+        <div class=" text-center mt-5 mb-5 md:text-base text-pramary">
             {text[textIndex][2]}
         </div>
         <div
@@ -95,7 +93,7 @@
                 class=" flex border border-main rounded-lg px-3 md:px-4 py-2 md:py-4"
             >
                 <div class=" flex flex-col text-main">
-                    <div class=" text-[12px] md:text-[14px]">
+                    <div class=" text-[14px]">
                         {text[textIndex][3]}
                     </div>
                     <div class=" text-3xl font-medium">340 +</div>
@@ -114,7 +112,7 @@
                 class=" flex border border-main rounded-lg px-3 md:px-4 py-2 md:py-4"
             >
                 <div class=" flex flex-col text-main">
-                    <div class=" text-[12px] md:text-[14px]">
+                    <div class=" text-[14px]">
                         {text[textIndex][4]}
                     </div>
                     <div class=" text-3xl font-medium">50 +</div>
@@ -133,11 +131,11 @@
                 </div>
             </div>
         </div>
-        <div class=" flex justify-center text-secondary mt-5 font-light">
+        <div class=" flex justify-center text-secondary mt-5">
             {text[textIndex][8]} 2024.6.18
         </div>
         <div
-            class=" flex flex-col px-10 md:px-48 mt-5 text-secondary font-light text-sm md:text-base gap-2"
+            class=" flex flex-col px-10 md:px-48 mt-5 text-secondary md:text-base gap-2"
         >
             <p>
                 {text[textIndex][6]}
